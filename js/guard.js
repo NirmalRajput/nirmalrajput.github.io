94dcb25d3fdcd5edcd702b80bc7708b33a79d5de
Nirmal Rajput <31390338+NirmalRajput@users.noreply.github.com>
2026-02-04 18:27:40 +0530

    Store current page in sessionStorage if granted

--- js/guard.js
+++ js/guard.js
@@ -1,5 +1,7 @@
 (function protect() {
   if (sessionStorage.getItem(SESSION_KEY) !== "granted") {
     window.location.href = "/";
+  } else {
+    sessionStorage.setItem(PAGE_KEY, window.location.pathname);
   }
 })();
