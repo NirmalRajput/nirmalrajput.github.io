c9293c4a55d1a22b99eeb2e6be1e302283712c3a
Nirmal Rajput <31390338+NirmalRajput@users.noreply.github.com>
2026-02-04 18:21:45 +0530

    Add authentication constants for session and emails

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+const SESSION_KEY = "soft-private-access";
+
+const ALLOWED_EMAILS = [
+  "nirmalrajput015@gmail.com",
+  "nirmalrajput228@gmail.com"
+];
+
+const PASSCODE_HASH =
+  "53ec406b1e1fdf01f250f19f2773a4441c166c8f2908ba5fe4f6fe4ebea96d78";
